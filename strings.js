251f555245cbb65704b76ad05fc8f1265d8afb36
Zachary Cline <zcline08@gmail.com>
2016-11-08 15:19:56 -0600

    Completed Requirements

--- strings.js
+++ strings.js
@@ -1,23 +1,19 @@
 
-//This keeps allows for only letters(lower and capital) to be input into the text box.
-document.getElementById("input1").onkeypress=function(e){
-    if(("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ").indexOf(String.fromCharCode(e.keyCode))===-1){
-        e.preventDefault();
-        return false;
-    }
-}
+let testString = "";
+var input = document.getElementById("input1");
+var button = document.getElementById("button1");
 
-var input = document.getElementById("input1").value;
 
 // Implement the logic in the reversal function that reverses the order of the characters in the string, and outputs the result in the DOM, below the text input.
 function reversal(testString) {
+	testString = input.value
 	var output = document.getElementById("output");
-
  return output.innerHTML=testString.split("").reverse().join("");
 
 }
 
-function alphabits(testString) {  
+function alphabits(testString) {
+	testString = input.value
 	var output = document.getElementById("output2");
 
 return output.innerHTML=testString.split("").sort().join("");  
@@ -27,7 +23,8 @@ return output.innerHTML=testString.split("").sort().join("");
 
 //This Function takes the string passed into it and splits it letter by letter, reverses the individual letters, and joins them back, then checks to see if it is exactly equal to the text within the teststring variable. If it is it will tell you that you have a palindrome. If not it will tell you it isnt.
 function palindrome(testString) {
-	 var ouput = document.getElementById("output3")
+	testString = input.value
+	 var output = document.getElementById("output3")
 	 var paliString = testString.split('').reverse().join('');
 	 if (paliString === testString){
 	 output.innerHTML ="Bruh, Your String is a Palindrome";
@@ -35,39 +32,34 @@ function palindrome(testString) {
 
 }
 
-var testString = input;
+// var testString = "Racecar";
 
 
 
-//When the button is pressed or the enter key is pressed... set the value of the input inside the variable testString
-//*********************************
-// reversal(testString);
+// When the button is pressed or the enter key is pressed... set the value of the input inside the variable testString
+// *********************************
+// reversal(input);
 // console.log("Reverse", reversal(testString));
 
-// //****************************************
-// alphabits(testString);
+//****************************************
+// alphabits(input);
 // console.log("Alphabit", alphabits);
 
 
-// palindrome(testString);
+// palindrome(input);
 // console.log("Palindrome", palindrome);
 
-function callEmAll (palindrome, alphabits, reversal, teststring){
-	palindrome(teststring)
-	alphabits(teststring)
-	reversal(teststring)
+
+
+document.getElementById("input1").onkeypress=function(e){
+    if(("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ").indexOf(String.fromCharCode(e.keyCode))===-1){
+        e.preventDefault();
+        return false;
+    }
 }
 
-
-var wordBtn = document.getElementById("button1");
-wordBtn.addEventlistener("click", callEmAll(palindrome, alphabits, reversal, testString));
-
-// When the user presses the enter key in the text input, or clicks the button, set the value of the testString variable (see below) to the value of the input.
-// The output of each of the functions should immediately appear as well
+button.addEventListener("click",alphabits);
+button.addEventListener("click",reversal);
+button.addEventListener("click",palindrome);
 
 
-//why do i want to create an object?
-
-// let red {
-	
-// }
